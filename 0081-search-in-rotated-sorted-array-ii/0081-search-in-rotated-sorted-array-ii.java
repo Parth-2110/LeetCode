d12579e1173a1a3edd5966653face794cb00d4ae
Parth Chaudhari <parthchaudhari3227@gmail.com>
2026-07-26 19:42:50 +0530

    Time: 0 ms (100%), Space: 44.7 MB (96.64%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right){
+
+            int mid = (left + right)/2;
+
+            if(nums[mid] == target){
+                return true;
+            }
+
+            if(nums[left] == nums[mid]){
+               left++;
+                continue;
+            }
+
+            else if(nums[mid] >= nums[left]){
+
+                if(nums[left] <= target && target <= nums[mid]){
+                    right = mid - 1;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+
+            else{
+                if(target <= nums[right] && target >= nums[mid]){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
