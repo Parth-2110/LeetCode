bdbfa7d95a7607a32e5a4fab24641435bc7ea124
Parth Chaudhari <parthchaudhari3227@gmail.com>
2026-08-20 10:23:05 +0530

    Time: 0 ms (100%), Space: 42.9 MB (84.55%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int left = Integer.MIN_VALUE;
+        for(int num: nums){
+            left = Math.max(left,num);
+
+        }
+
+        int right = 0;
+        for(int num:nums){
+            right += num;
+        }
+
+        int ans = 0;
+        while(left <= right){
+
+            int mid = left + (right - left)/2;
+
+            if(isItPossible(nums,k,mid)){
+                right = mid - 1;
+                ans = mid;
+            }
+
+            else{
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public boolean isItPossible(int[] nums, int k, int mid){
+
+        int sum = 0;
+        int partition = 1;
+
+        for(int num: nums){
+
+            if(sum + num > mid){
+                partition++;
+                sum = num;
+            }
+
+            else{
+                sum += num;
+            }
+        }
+
+        if(partition <= k){
+            return true;
+        }
+
+        return false;
+    }
+
+}
