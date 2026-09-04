a5a0952bffa73f5df9689d14991acd585a83b343
Parth Chaudhari <parthchaudhari3227@gmail.com>
2026-09-04 23:24:05 +0530

    Time: 26 ms (5.31%), Space: 43.8 MB (58.7%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,22 +1,31 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        
-        HashMap<Character,Integer> Smap = new HashMap<>();
-        HashMap<Character,Integer> Tmap = new HashMap<>();
 
-        for(int i = 0; i < s.length(); i++){
+        if (s.length() != t.length()) {
+            return false;
+        }
 
-            if(!Smap.containsKey(s.charAt(i))){
-                Smap.put(s.charAt(i), i);
-            }
+        HashMap<Character, Character> SMap = new HashMap<>();
+        HashMap<Character, Character> TMap = new HashMap<>();
 
-            if(!Tmap.containsKey(t.charAt(i))){
-                Tmap.put(t.charAt(i), i);
-            }
+        for (int i = 0; i < s.length(); i++) {
 
-            if(!Smap.get(s.charAt(i)).equals(Tmap.get(t.charAt(i)))){
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+
+            // Check s -> t mapping
+            if (SMap.containsKey(a) && SMap.get(a) != b) {
                 return false;
             }
+
+            // Check t -> s mapping
+            if (TMap.containsKey(b) && TMap.get(b) != a) {
+                return false;
+            }
+
+            // Create the mappings
+            SMap.put(a, b);
+            TMap.put(b, a);
         }
 
         return true;
