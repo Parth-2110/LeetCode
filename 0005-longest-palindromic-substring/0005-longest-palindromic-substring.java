3128671733b09153a191f53c8f0a59bc144663b4
Parth Chaudhari <parthchaudhari3227@gmail.com>
2026-09-05 23:16:22 +0530

    Time: 19 ms (40.7%), Space: 47.1 MB (17.16%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,40 @@
-public class Solution {
+class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
+
+        if(s.length() <= 1){
             return s;
         }
 
-        String maxStr = s.substring(0, 1);
+        String maxStr = s.substring(0,1); //Take the first character of s and assume it is currently the longest palindrome.So we know that maxStr starts with a valid palindrome.
 
-        for (int i = 0; i < s.length() - 1; i++) {
-            String odd = expandFromCenter(s, i, i);
-            String even = expandFromCenter(s, i, i + 1);
+        for(int i = 0; i < s.length(); i++){
 
-            if (odd.length() > maxStr.length()) {
+            String odd = expandFromCentre(s,i,i);
+            String even = expandFromCentre(s,i,i+1);
+
+            if(odd.length() > maxStr.length()){
                 maxStr = odd;
             }
-            if (even.length() > maxStr.length()) {
+
+            if(even.length() > maxStr.length()){
                 maxStr = even;
             }
-        }
+        }  
 
-        return maxStr;
+        return maxStr;      
     }
 
-    private String expandFromCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-        return s.substring(left + 1, right);
+
+
+    public String expandFromCentre(String s, int left , int right){
+
+        while((left >= 0 && right < s.length()) && ((s.charAt(left)) == (s.charAt(right))))
+{   
+    left--;
+    right++;
+}
+
+return s.substring(left + 1,right);
+
     }
 }
